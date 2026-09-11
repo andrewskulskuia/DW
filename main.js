b70d99cf0ef3baef8b803c6c7d9d5e726ad87aa4
andrewskulskuia <andrewskulskyia@gmail.com>
2026-09-11 18:27:16 +0300

    WIP

---
+++ main.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const moment = require('moment')
+const app = express()
+const PORT = 3000
+const HOST = "localhost"
+
+app.get('/timestamp', (req, res) => {
+  res.json({
+    time: moment().format("HH:mm:ss"),
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`http://${HOST}:${PORT}`)
+});
